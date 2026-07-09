30d1cdb4436d864342e681eb30da9a06aaa6a160
Renee <chung_huing@yahoo.com.tw>
2026-07-09 10:29:08 +0800

    feat: setup project foundation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <HelloWorld />
+  <div>員工請假系統</div>
 </template>
